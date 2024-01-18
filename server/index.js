eca0341006694f4fe9a86bf3b905e35d27e659fe
highbridgejiko <highbridgejiko@gmail.com>
2024-01-18 15:09:01 +0900

    ✨ [ユーザ新規登録API]パスワード暗号化処理とユーザ新規登録処理を作成

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5050;
-const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 //DB接続
@@ -13,7 +16,17 @@ try {
 }
 
 //ユーザー新規登録API
+app.post("/register", async (req, res) => {
+  //パスワードの受け取り
+  const password = req.body.password;
 
+  try {
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成
+    const user = await User.create(req.body);
+  } catch {}
+});
 //ユーザログイン用API
 
 app.listen(PORT, () => {
