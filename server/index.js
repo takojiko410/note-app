becc15f8c891a06579f0c86c9abd41ad2922146e
highbridgejiko <highbridgejiko@gmail.com>
2024-01-19 00:29:00 +0900

    ✨  [ユーザ新規登録API] JWTトークンの発行処理を実装

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
 const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5050;
@@ -25,7 +26,14 @@ app.post("/register", async (req, res) => {
     req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
     //ユーザーの新規作成
     const user = await User.create(req.body);
-  } catch {}
+    //JWTの発行
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 //ユーザログイン用API
 
