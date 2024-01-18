3d8040a56be545a74253595d23779abf93877eac
highbridgejiko <highbridgejiko@gmail.com>
2024-01-18 14:42:05 +0900

    🔐 DB接続を確認し、.envファイルを作成

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 5050;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+//DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("DBと接続中・・・");
+} catch (error) {
+  console.log(error);
+}
 
 //ユーザー新規登録API
 
