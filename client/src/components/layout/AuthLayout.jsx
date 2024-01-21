e3f70d4754b75cf16cd03039dd64b66f4016f9ea
highbridgejiko <highbridgejiko@gmail.com>
2024-01-21 13:17:27 +0900

    ✨ トークン認証APIを叩く機能を実装

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,10 +1,26 @@
 import { Box } from "@mui/material";
 import {Container} from "@mui/system";
 import React from 'react'
-import { Outlet } from 'react-router'
+import {useEffect} from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Logo from "../../assets/images/logo.png";
+import authUtils from "../../utils/authUtils";
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //JWTを持っているのか確認する
+    const checkAuth = async () => {
+      //認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if(isAuth) {
+        navigate("/");
+      }
+    }
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
