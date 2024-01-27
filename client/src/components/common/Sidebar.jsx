744cbe9dea41c5a20aabf2ab7af7a791e203b7ea
highbridgejiko <highbridgejiko@gmail.com>
2024-01-27 21:03:24 +0900

    ✨ テスト出力用のメモデータを作成

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -3,7 +3,7 @@ import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined"
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined"
 import React from 'react'
 import assets from '../../assets/index'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector} from "react-redux";
 
 const Sidebar = () => {
@@ -51,6 +51,21 @@ const Sidebar = () => {
               <AddBoxOutlinedIcon fontSize="small" />
             </IconButton>
           </Box>
+        </ListItemButton>
+        <ListItemButton sx={{pl: "20px"}} component={Link} to="/memo/fkasj24132fjas">
+          <Typography>
+            📝仮置きのメモ
+          </Typography>
+        </ListItemButton>
+        <ListItemButton sx={{pl: "20px"}} component={Link} to="/memo/fkasj24132fjas">
+          <Typography>
+            📝仮置きのメモ
+          </Typography>
+        </ListItemButton>
+        <ListItemButton sx={{pl: "20px"}} component={Link} to="/memo/fkasj24132fjas">
+          <Typography>
+            📝仮置きのメモ
+          </Typography>
         </ListItemButton>
       </List>
     </Drawer>
