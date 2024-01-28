ef06ede9222190e902d9e0564045e48eff5b73e8
highbridgejiko <highbridgejiko@gmail.com>
2024-01-28 22:31:57 +0900

    ✨ １つのメモを取得するAPIを実装

--- server/src/v1/routes/memo.js
+++ server/src/v1/routes/memo.js
@@ -8,4 +8,7 @@ router.post("/", tokenHandler.verifyToken, memoController.create);
 //ログインしているユーザが投稿したメモを全て取得
 router.get("/", tokenHandler.verifyToken, memoController.getAll);
 
+//ログインしているユーザが投稿したメモを1つ取得
+router.get("/:memo", tokenHandler.verifyToken, memoController.getOne);
+
 module.exports = router;
