c45824b0a55d856d929912f94e5b8c7222d41f9a
highbridgejiko <highbridgejiko@gmail.com>
2024-01-28 22:55:36 +0900

    💄 メモコンポーネントを作成

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,8 +1,39 @@
+import { IconButton, TextField } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined"
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined"
 
 const Memo = () => {
   return (
-    <div>Memo</div>
+    <>
+      <Box sx={{display: "flex", alignItems: "center", width: "100",}}>
+        <IconButton>
+          <StarBorderOutlinedIcon/>
+        </IconButton>
+        <IconButton variant="outlined" color="error">
+          <DeleteOutlinedIcon/>
+        </IconButton>
+      </Box>
+      <Box sx={{padding: "10px 50px"}}>
+        <TextField
+          placeholder='無題'
+          variant='outlined'
+          fullWidth
+          sx={{".MuiOutlinedInput-input": { padding: 0 },
+               ".MuiOutlinedInput-notchedOutline": {border: "none" },
+               ".MuiOutlinedInput-root": {fontSize: "2rem", fontWeight: "700"}}}
+
+        />
+        <TextField
+          sx={{".MuiOutlinedInput-input": { padding: 0 },
+          ".MuiOutlinedInput-notchedOutline": {border: "none" },
+          ".MuiOutlinedInput-root": {fontSize: "1rem"}}}
+          placeholder='追加'
+          variant='outlined'
+          fullWidth />
+      </Box>
+    </>
   )
 }
 
